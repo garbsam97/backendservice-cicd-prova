f2549336fb3cbb59c8960bf26058f4288c6b93d4
Samuele Garbuglia <samuele.garbuglia@cimatti.it>
2023-09-09 18:12:03 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Hello K8S CI/CD</title>
+	</head>
+	<body>
+		<h1>Benvenuto</h1>
+		<div>
+			Prova CI/CD su Kubernetes
+		</div>
+		<div>
+			<small>
+				<?php
+					$date = date('d-m-Y');
+					echo "Today is: " . $date;
+				?>
+			</small>
+		</div>
+	</body>
+</html>
